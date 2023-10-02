7635de368bda85f8e9ff91a9af56d07367a1f85f
Arbia Bougamra <arbia.bougamra@gmail.com>
2023-10-02 01:05:05 +0100

    fixed issue

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    httpPort: 27027,
-    mongoUri: "mongodb://localhost:27017/salveo"
+    httpPort: 27017,
+    mongoUri: "mongodb://127.0.0.1:27017/"
 };
 
 module.exports = config;
