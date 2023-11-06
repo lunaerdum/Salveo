9a5dcf4fd3a2c92b3678573b64ccfef88b96f20a
Arbia Bougamra <arbia.bougamra@gmail.com>
2023-11-06 14:51:59 +0100

    Custom Errorhandler + Setting up user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,49 @@
+import asyncHandler from 'express-async-handler';
+
 // @desc Auth user | Set token
 // route POST /api/users/auth
 // @access Public
 
-import asyncHandler from 'express-async-handler';
 const authUser = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Auth User'});
 });
 
-export { authUser };
+// @desc Register a new user
+// route POST /api/users
+// @access Public
+
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Register User'});
+});
+
+// @desc Logout user
+// route POST /api/users/logout
+// @access Public
+
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Logout User'});
+});
+
+// @desc Get user profile
+// route GET /api/users/logout
+// @access Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'User profile'});
+});
+
+// @desc Update user profile
+// route PUT /api/users/logout
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({message: 'Update user profile'});
+});
+
+export { 
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+};
