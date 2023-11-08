9ee76df69b560717cd771e0df1f8893d58dd81e6
Arbia Bougamra <arbia.bougamra@gmail.com>
2023-11-09 00:40:59 +0100

    setting the cookie along with logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -65,7 +65,11 @@ const registerUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const logoutUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Logout User'});
+    res.cookie('jwt', '', {
+        httpOnly: true,
+        expires: new Date(0)
+    })
+    res.status(200).json({message: 'User logged out'});
 });
 
 // @desc Get user profile
