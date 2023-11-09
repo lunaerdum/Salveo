cf989e8e8566a18f8f18052c072214956ff1228d
Arbia Bougamra <arbia.bougamra@gmail.com>
2023-11-09 01:36:13 +0100

    update profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,13 +11,13 @@ const authUser = asyncHandler(async (req, res) => {
 
     const user = await User.findOne({ email });
 
-    if (user && (await user.matchPassword(password))){
+    if (user && (await user.matchPassword(password))) {
         generateToken(res, user._id);
         res.status(201).json({
             _id: user._id,
             username: user.username,
             email: user.email
-    });
+        });
     } else {
         res.status(401);
         throw new Error('Invalid email or password');
@@ -34,7 +34,7 @@ const registerUser = asyncHandler(async (req, res) => {
 
     const userExists = await User.findOne({ email })
 
-    if(userExists) {
+    if (userExists) {
         res.status(400);
         throw new Error('User already exists');
     }
@@ -45,19 +45,19 @@ const registerUser = asyncHandler(async (req, res) => {
         password,
     });
 
-    if (user){
+    if (user) {
         generateToken(res, user._id);
         res.status(201).json({
             _id: user._id,
             username: user.username,
             email: user.email
-    });
+        });
     } else {
         res.status(400);
         throw new Error('Invalid user data');
     }
 
-    res.status(200).json({message: 'Register User'});
+    res.status(200).json({ message: 'Register User' });
 });
 
 // @desc Logout user
@@ -69,7 +69,7 @@ const logoutUser = asyncHandler(async (req, res) => {
         httpOnly: true,
         expires: new Date(0)
     })
-    res.status(200).json({message: 'User logged out'});
+    res.status(200).json({ message: 'User logged out' });
 });
 
 // @desc Get user profile
@@ -77,7 +77,13 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @access Private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'User profile'});
+
+    const user = {
+        _id: req.user._id,
+        username: req.user.username,
+        email: req.user.email
+    }
+    res.status(200).json(user);
 });
 
 // @desc Update user profile
@@ -85,10 +91,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @access Private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Update user profile'});
+    const user = await User.findById(req.user._id);
+
+    if (user) {
+        user.username = req.body.username || user.username;
+        user.email = req.body.email || user.email;
+
+        if (req.body.password) {
+            user.password = req.body.password;
+        }
+
+        const updatedUser = await user.save();
+
+        res.status(200).json({
+            _id: updatedUser._id,
+            username: updatedUser.username,
+            email: updatedUser.email,
+        });
+        
+    } else {
+        res.status(404);
+        throw new Error('User not found');
+    }
+    res.status(200).json(user);
 });
 
-export { 
+export {
     authUser,
     registerUser,
     logoutUser,
